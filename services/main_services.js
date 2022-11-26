647ec530fe5eb751a2458e13dd039566dd21f719
felip2018 <felipegarxon@hotmail.com>
2022-11-25 23:46:58 -0500

    :100: handle responses

--- services/main_services.js
+++ services/main_services.js
@@ -4,12 +4,12 @@ const login = () => {
     let clave = document.getElementById('clave').value;
     let resultado = document.getElementById('login_result');
     resultado.innerHTML = '';
-    if(usuario === "") {
+    if (usuario === "") {
         resultado.innerHTML = '<div class="alert alert-warning">Ingrese el usuario</div>';
         return;
     }
 
-    if(clave === "") {
+    if (clave === "") {
         resultado.innerHTML = `<div class="alert alert-warning">Ingrese la contraseña</div>`;
         return;
     }
@@ -17,16 +17,23 @@ const login = () => {
     fetch('https://hfo87sow46.execute-api.us-east-1.amazonaws.com/v1/uniminuto_services/login', {
         method: "POST",
         headers: {
-            "Content-type": "application/json;charset=UTF-8"
+            "Content-type": "application/json;charset=UTF-8",
+            "Accept": "*/*",
         },
-        body: {
+        body: JSON.stringify({
             usuario,
             clave
-        }
+        })
     })
     .then(response => response.json()) 
     .then(json => {
-        console.log("LOGIN RES:: ",json)
+        let message;
+        if (json.validation) {
+            message = `<div class="alert alert-success">¡Se ha iniciado sesión correctamente!</div>`
+        } else {
+            message = `<div class="alert alert-danger">¡No ha sido posible iniciar sesión, verifique usuario y/o contraseña!</div>`
+        }
+        resultado.innerHTML = message;
     })
     .catch((error) => {
         console.error('Something was wrong with login service > ',error);
@@ -36,11 +43,23 @@ const login = () => {
 
 const getPokemonsList = () => {
     console.log('This is the get pokemons list service');
+    let resultado = document.getElementById('pokemons_result');
+    resultado.innerHTML = '';
+
     fetch('https://hfo87sow46.execute-api.us-east-1.amazonaws.com/v1/uniminuto_services/get-pokemons', {
-        method: 'GET'
+        method: "GET",
+        headers: {
+            "Content-type": "application/json;charset=UTF-8",
+            "Accept": "*/*",
+        },
     })
-    .then((res) => {
-        console.log("POKEMONS RES:: ",res);
+    .then(response => response.json()) 
+    .then(json => {
+        let list = '';
+        json.result.list.forEach(element => {
+            list += `<li>${element.name}</li>`;
+        });
+        resultado.innerHTML = list;
     })
     .catch((error) => {
         console.error('Something was wrong with pokemons service > ',error);
